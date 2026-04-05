f204a5718f01be3a310c9c7cbb824534d055ead9
Sebatian Luis Enrique Aguilar Diaz <sebastian.luis.aguilar.diaz@gmail.com>
2026-04-05 16:25:45 -0600

    Class Board on process

--- src/main/java/com/github/sebastianaguilardiaz/Board.java
+++ src/main/java/com/github/sebastianaguilardiaz/Board.java
@@ -1,14 +1,52 @@
 package com.github.sebastianaguilardiaz;
 
+
 public class Board {
 
     public static final int width=5;
     public static final int height=2;
 
 
-    Card matrix[][]=new Card [Board.height][Board.width];
+    public Card matrix[][]=new Card [Board.height][Board.width];
 
-    
+    // its true if the card is upwards
+    public boolean upwardsCards[][]=new boolean [Board.height][Board.width];
+
+    public int numOfUpwardsCards=0;
+
+
+
+    public Board(Card m[][]){
+
+        
+        for(int i=0;i<Board.height;i++){
+            for(int j=0;j<Board.width;j++){
+                matrix[i][j]=m[i][j];
+                // the cards must be upwards at the beginning 
+                upwardsCards[i][j]=false;
+            }
+        }
+    }
+
+
+    public void printBoard(){
+        for(int row=0; row < Board.height; row++){
+            for(int cardRow=0;cardRow<Card.height;cardRow++){
+                for(int column=0;column<Board.width;column++){
+                    for(int cardColumn=0;cardColumn<Card.width;cardColumn++){
+
+                        // if the card is upwards it prints its front face else, prints the back face
+                        if (upwardsCards[row][column]) System.out.print(this.matrix[row][column].front[cardRow][cardColumn]);
+                        else System.out.println(Card.back[cardRow][cardColumn]);
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+
+
 
 
 }
