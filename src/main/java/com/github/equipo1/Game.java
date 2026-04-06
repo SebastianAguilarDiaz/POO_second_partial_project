7077b0a2a659df04d6f863a3a8b968262ae5fa88
Sebastian Luis Enrique Aguilar Díaz <sebastian.luis.aguilar.diaz@gmail.com>
2026-04-05 22:03:29 -0600

    askForWords added on Game class

--- src/main/java/com/github/equipo1/Game.java
+++ src/main/java/com/github/equipo1/Game.java
@@ -3,10 +3,15 @@ package com.github.equipo1;
 import java.util.Scanner;
 
 public class Game {
+    private String words[]=new String [5];
+    private Card cards[][]=new Card[Board.height][Board.width];
+    private Scanner sc;
 
-    Game(Player players[], Board board,Scanner sc){
 
+    Game(Player players[], Board board,Scanner s){
+        this.sc=s;
 
+        this.askForTheWords();
         
         
         int pairsPerPlayer[]= new int [2];
@@ -15,7 +20,7 @@ public class Game {
 
         // it iterates the player and ends only when the number of cards upwards its 10
         for(int i=0;i<2 && board.numOfUpwardsCards<10;i++){
-            Turn actualTurn= new Turn(players[i],board,sc);
+            Turn actualTurn= new Turn(players[i],board,this.sc);
             
             // if the actual player succed on his turn, it means his guess had been correct
             if(actualTurn.succeed)pairsPerPlayer[i]++;
@@ -33,4 +38,29 @@ public class Game {
 
     }
 
+    private void askForTheWords(){
+
+        System.out.println("Ingresen 5 palabras de 6 letras");
+
+
+        for(int i=0; i< 5; i++){
+            String x=this.sc.nextLine();
+            
+            // if the word hasn't the necesarry length we skip it
+            if (x.length()!=6){
+                System.out.println("El tamano de la palabra debe ser 6 letras");
+                i--;
+                
+            }
+            // else we save it and continue
+            else
+                words[i]=x;
+        }
+
+    }
+
+    private void createCards(){
+
+        
+    }
 }
