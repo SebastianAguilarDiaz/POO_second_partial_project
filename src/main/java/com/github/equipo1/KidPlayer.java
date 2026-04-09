a4488ae6427c606851baa3d478435610d0588c8d
YazhwhanMorales <developeryazhwhan@gmail.com>
2026-04-08 19:52:25 -0600

    New Fail and congrats phrases added ( five for each player type)

--- src/main/java/com/github/equipo1/KidPlayer.java
+++ src/main/java/com/github/equipo1/KidPlayer.java
@@ -1,8 +1,23 @@
 package com.github.equipo1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class KidPlayer extends Player{
+
+    private Random random = new Random();
+    private  String failPhrases[] = {
+            "no pasa nada si te equivocas, asi es como aprendemos, confia en ti y veras que pronto lo lograras!",
+            "eres mas capaz de lo que crees, solo tienes que confiar en ti, sigue intentando!",
+            "esta vez fallaste, pero a la proxima estoy seguro de que lo lograras!",
+            "no te rindas! eres genial por intentarlo cada error te acerca mas a la victoria!",
+            "no te preocupes por no haber encontrado la pareja en este turno porque estas haciendo un trabajo realmente increible y estoy completamente seguro de que en el siguiente intento recordaras perfectamente donde estan todas las cartas para ganar el juego"};
+    private  String congratsPhrases[] = {
+            "tienes una memoria realmente asombrosa y envidiable porque acabas de encontrar una pareja de cartas demostrando que pusiste muchisima atencion a cada uno de los movimientos en el tablero de juego sigue asi para ser el mejor jugador",
+            "eres alguien super genial! sigue asi y llegaras a ser el mejor en este juego!",
+            "lo lograste esta vez! asi que puedes hacerlo otra vez, confia en ti! cada logro es una victoria para tu persona!",
+            "Eres la persona mas brillante que he conocido, sigue asi y veras como este juego pronto sera tuyo! Domina la partida! ",
+            "Cada vez que lo logras estoy mas feliz por ti, cada vez que lo logras estoy mas seguro que puedes lograr todas las metas que te propongas!"};
 
     public KidPlayer(Scanner sc, String name, int age){
         super(sc,name,age);
@@ -14,16 +29,16 @@ public class KidPlayer extends Player{
     // prints the motivational phrase in case that the player failed
     @Override
     public void printMotivationalPhrase(){
-
-        System.out.println(this.getName() + ", no te preocupes por no haber encontrado la pareja en este turno porque estas haciendo un trabajo realmente increible y estoy completamente seguro de que en el siguiente intento recordaras perfectamente donde estan todas las cartas para ganar el juego");
+        int randomP = random.nextInt(failPhrases.length);
+        System.out.println(this.getName() + ", " + failPhrases[randomP]);
     
     }
 
     // prints the congrats phrase in case that the player succeeded
     @Override
     public void printCongratsPhrase(){
-
-        System.out.println("Felicidades " + this.getName() + ", tienes una memoria realmente asombrosa y envidiable porque acabas de encontrar una pareja de cartas demostrando que pusiste muchisima atencion a cada uno de los movimientos en el tablero de juego sigue asi para ser el mejor jugador");
+        int randomP = random.nextInt(congratsPhrases.length);
+        System.out.println("Felicidades " + this.getName() + ", " + congratsPhrases[randomP]);
     
     }
 
